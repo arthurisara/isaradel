ac9ea533b6a19fc42339324c1e97f246be787a40
Arthur <adeloche@etu.isara.fr>
2024-02-09 11:51:49 +0100

    testshiny.R

--- testshiny.R
+++ testshiny.R
@@ -1,21 +1,17 @@
-install.packages("shiny")
 # Charger le package Shiny
 library(shiny)
 
-# Définir l'interface utilisateur (IU)
-iu <- fluidPage(
-  # Titre de l'application
-  titlePanel("Calcul avec Puissance"),
+# Définir l'interface utilisateur (UI)
+ui <- fluidPage(
+  titlePanel("Calculateur Puissant"),
   
-  # Entrée : Boîte de texte pour saisir un nombre
   sidebarLayout(
     sidebarPanel(
-      textInput("number", "Entrez un nombre :", value = ""),
+      textInput("number", "Entrez un nombre :", value = "5"),
       sliderInput("power", "Choisissez une puissance :", min = 1, max = 10, value = 2),
       actionButton("calculate", "Calculer")
     ),
     mainPanel(
-      # Résultat : Affiche le résultat du calcul
       verbatimTextOutput("result")
     )
   )
@@ -23,25 +19,17 @@ iu <- fluidPage(
 
 # Définir le serveur
 server <- function(input, output) {
-  # Réaction pour effectuer le calcul lorsque le bouton est cliqué
   observeEvent(input$calculate, {
-    # Obtenir le nombre saisi par l'utilisateur
     input_number <- as.numeric(input$number)
     
-    # Vérifier si l'entrée est un nombre valide
     if (!is.na(input_number)) {
-      # Obtenir la puissance choisie par l'utilisateur
       input_power <- as.numeric(input$power)
-      
-      # Calculer le résultat
       result <- input_number^input_power
       
-      # Afficher le résultat dans l'output "result"
       output$result <- renderPrint({
         paste("Le résultat de", input_number, "élevé à la puissance", input_power, "est", result)
       })
     } else {
-      # Afficher un message d'erreur si l'entrée n'est pas valide
       output$result <- renderPrint({
         "Veuillez entrer un nombre valide."
       })
@@ -50,4 +38,5 @@ server <- function(input, output) {
 }
 
 # Créer l'application Shiny
-shinyApp(iu, server)
+shinyApp(ui, server)
+
